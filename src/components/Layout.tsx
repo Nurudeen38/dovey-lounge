3f102f183caf45af46e113c1224ccbee4eab4d6c
unknown <nurudeenkeen@gmail.com>
2026-01-28 17:30:48 +0100

    build add analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = () => {
     return (
@@ -9,6 +10,7 @@ const Layout = () => {
             <Header />
             <Box component="main">
                 <Outlet />
+                <Analytics />
             </Box>
             <Footer />
         </Box>
