311612e10c553056ee7febce008dc9a18579d86e
unknown <nurudeenkeen@gmail.com>
2026-01-28 12:28:32 +0100

    chore: change hero background

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -6,9 +6,10 @@ export interface HeroSlideMedia {
 }
 
 export const HERO_SLIDES: HeroSlideMedia[] = [
+    { type: 'video', src: '/assets/video_1.mp4', poster: '/assets/nail_parlour.jpg' },
+    { type: 'video', src: '/assets/video_2.mp4', poster: '/assets/nail_parlour.jpg' },
     { type: 'image', src: '/assets/guest_reception.jpg' },
     { type: 'image', src: '/assets/saloon_entrance.jpg' },
     { type: 'image', src: '/assets/nail_parlour.jpg' },
-    { type: 'video', src: '/assets/video_1.mp4', poster: '/assets/nail_parlour.jpg' },
-    { type: 'video', src: '/assets/video_2.mp4', poster: '/assets/nail_parlour.jpg' },
+
 ];
